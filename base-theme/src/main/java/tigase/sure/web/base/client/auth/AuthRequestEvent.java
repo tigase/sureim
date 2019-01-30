4a8fef7836adfa976c96d7541dd6834ceced6dda
Wojciech Kapcia <wojtek@tigase.org>
2019-01-30 16:14:36 -0300

    #8483 update licence headers

--- base-theme/src/main/java/tigase/sure/web/base/client/auth/AuthRequestEvent.java
+++ base-theme/src/main/java/tigase/sure/web/base/client/auth/AuthRequestEvent.java
@@ -1,13 +1,10 @@
-/*
- * AuthRequestEvent.java
- *
- * Tigase XMPP Web Client
- * Copyright (C) 2012-2017 "Tigase, Inc." <office@tigase.com>
+/**
+ * Sure.IM base theme library - bootstrap configuration for all Tigase projects
+ * Copyright (C) 2012 Tigase, Inc. (office@tigase.com)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License,
- * or (at your option) any later version.
+ * the Free Software Foundation, version 3 of the License.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
